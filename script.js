85ae8ee9ee34c2aa744cb1bc8878b1a5c1fced79
samhhenderson <yourname@example.com>
2022-06-16 13:09:13 -0700

    Modify .js logic. Finish writing numbers logic. Incomplete math logic.

--- script.js
+++ script.js
@@ -1,54 +1,89 @@
-//var dataArray = new Array[{previousValue:``}, {currentValue:``}, {operator:``}]
+//var dataArray = new Array[{num1:``}, {num2:``}, {operator:``}]
 
-var previousValue = 0;
-var currentValue = ``;
-var activeOperator = ``;
+var num1 = `0`;
+var num2 = `0`;
+var tempOperator = `none`;
+var operator = `none`;
+var calcStatus = `clear`;
 
 const display = document.querySelector(`.display`);
 
-
-
-window.add = function(a, b) {
-    console.log(a)
-    console.log(b)
-    return a + b;
+window.allClear = () => {
+    num1 = `0`;
+    num2 = `0`;
+    operator = `none`;
+    tempOperator = `none`;
+    writeDisplay(num1);
 }
 
-window.subtract = function(a, b) { 
-    return a - b;
+window.deleteLast = () => {
+    num1 = num1.slice(0, -1);
+    if (num1 === ``) num1 = `0`;
+    writeDisplay(num1);
 }
 
-window.equals = function(a, b) { 
-    return;
-}
-
-window.allClear = function(a, b) {
-    previousValue = 0;
-    currentValue = ``;
-    activeOperator = ``;
-    return ``;
+window.add = (a, b) => a + b;
+window.subtract = (a, b) => a - b;
+window.multiply = (a, b) => a * b;
+window.divide = (a, b) => {
+    if (b == 0) return `Please don't do that`;
+    else return a / b;
 }
 
 //Main function that handles user interaction
 const handleClicks = function(event) {
     const buttonClass = event.target.className;
     const buttonID = event.target.id;
-    switch(buttonClass) {
+    switch (buttonClass) {
         case `numbers`:
-            currentValue = `${currentValue}${buttonID}`;
-            writeDisplay(currentValue);
+            switch (buttonID) {
+                case `.`:
+                    if (num1.includes(`.`)) return;
+                    num1 = `${num1}${buttonID}`
+                    break;
+                case (`+/-`):
+                    if (num1 == `0`) return;
+                    if (num1.includes(`-`)) num1 = num1.slice(1);
+                    else num1 = `-${num1}`;
+                    break;
+                default: 
+                    if (num1 == `0`) num1 = ``;
+                    num1 = `${num1}${buttonID}`;
+                    break;
+            }
+            writeDisplay(num1);
+            /* operator = tempOperator;
+            tempOperator = `none`; */
+            calcStatus = ready;
             break;
-        case `operators`: 
-            if (activeOperator === buttonID) return;
-            if (activeOperator === ``) activeOperator = `add`;
-            previousValue = parseInt(previousValue, 10);
-            currentValue = parseInt(currentValue, 10);
-            writeDisplay(window[activeOperator](previousValue, currentValue))
-            previousValue = currentValue;
-            currentValue = ``;
-            activeOperator = buttonID;
+
+        case `operators`:
+            //handle the first entries after a clear
+            if (operator === `none` && tempOperator === `none`) {
+                tempOperator = buttonID;
+
+                num2 = num1;
+                num1 = `0`;
+                break;
+            }
+            else if (operator === `none`) {
+                tempOperator = buttonID;
+                break;
+            }
+            num1 = Number(num1);
+            num2 = Number(num2);
+            num2 = window[operator](num2, num1);
+            writeDisplay(num2);
+            tempOperator = buttonID;
+            operator = `none`;
+            num1 = '0';
             break;
+        case `dataMgt`:
+            window[buttonID]();
+            break;
+
     }
+
 }
 
 
@@ -61,7 +96,7 @@ const writeDisplay = function(input) {
 document.querySelectorAll(`.buttons`).forEach((button) => {
     button.addEventListener('click', (event) => handleClicks(event))
 })
-
+writeDisplay(num1);
 //1. When clicked, store the value of the button
 //2. Display the value of the button
 //3. Perform the desired operations when = is pressed
